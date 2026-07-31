16a98ed2e11f6d70d467da46b39da5ec53757a92
Andrew Shabailov <andrewshabailov@gmail.com>
2026-07-31 22:45:30 +0300

    fixed

--- module_3.py
+++ module_3.py
@@ -4,33 +4,32 @@ class Car:
         self.price = price
 
     def get_final_price(self):
-        if self.color == 'red':
-            return f'Цена машины красного цвета {int(self.price * 1.15)}'
+        if self.color == 'красный':
+            return int(self.price * 1.15)
         else:
-            return f'Цена машины: {int(self.price)}'
+            return int(self.price)
 
 
 class NewCar(Car):
-    def __init__(self, price, has_trailer):
-        self.price = price
+    def __init__(self, color, price, has_trailer):
+        super().__init__(color, price)
         self.has_trailer = has_trailer
 
-
     def get_final_price(self):
+        base = super().get_final_price()
         if self.has_trailer:
-            final_price = (self.price * 1.15) + (self.price * 0.35)
-            return f"Цена машины с трейлером: {int(final_price)}"
-        else:
-            return f'Цена машины: {int(self.price)}'
+            return int(base + self.price * 0.35)
+        return int(base)
 
 
-car1 = Car('red', 100)
-car2 = Car('green', 100)
-car3 = NewCar(100, True)
-car4 = NewCar(100, False)
+car1 = Car('красный', 100)
+car2 = Car('зеленый', 100)
+car3 = NewCar('синий', 100, False)
+car4 = NewCar('красный', 100, True)
 
 
-print(car1.get_final_price())
-print(car2.get_final_price())
-print(car3.get_final_price())
-print(car4.get_final_price())
+
+print("Машина красного цвета: ", car1.get_final_price())
+print("Машина зеленого цвета: ", car2.get_final_price())
+print("Машина синего цвета без прицепа: ", car3.get_final_price())
+print("Машина красного цвета с прицепом: ", car4.get_final_price())
